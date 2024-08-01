1b86f5e054df4523980bd5b9a1232e5f83359563
smugytea4real <thomasandromeda999@gmail.com>
2024-08-01 16:12:19 +0700

    Add Tailwind

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+            'resources/**/*.blade.php',
+            'resources/**/*.js',
+            'resources/**/*.vue',
             'resources/css/app.css',
             'resources/js/app.js',
             'resources/css/filament/admin/theme.css'
