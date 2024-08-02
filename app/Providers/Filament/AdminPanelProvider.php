a193ae1812854b32a94d9cd3437fb586256ed7af
smugytea4real <thomasandromeda999@gmail.com>
2024-08-02 11:33:32 +0700

    Role & Permission

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,14 +6,23 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
+use Filament\Pages\Dashboard;
 use Filament\Facades\Filament;
 use Filament\Support\Colors\Color;
 use SebastianBergmann\Type\VoidType;
 use Filament\Navigation\UserMenuItem;
+use Filament\Navigation\NavigationItem;
+use Filament\Navigation\NavigationGroup;
 use Filament\Http\Middleware\Authenticate;
+use Filament\Navigation\NavigationBuilder;
 use App\Filament\Resources\PeriodeResource;
+use App\Filament\Resources\StudentResource;
+use App\Filament\Resources\SubjectResource;
+use App\Filament\Resources\TeacherResource;
+use App\Filament\Resources\DepartmentResource;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
+use App\Filament\Resources\CategoryNilaiResource;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
@@ -21,6 +30,7 @@ use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -64,8 +74,55 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->viteTheme('resources/css/filament/admin/theme.css');
+            ->viteTheme('resources/css/filament/admin/theme.css')
+            ->plugin(FilamentSpatieRolesPermissionsPlugin::make())
+            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
+                return $builder->groups([
+                    NavigationGroup::make()
+                        ->items([
+                            NavigationItem::make('Dashboard')
+                            ->icon('heroicon-o-home')
+                            ->isActiveWhen(fn (): bool => request()->routeIs('filament.admin.pages.dashboard'))
+                            ->url(fn (): string => Dashboard::getUrl()),
+                        ]),
+                    NavigationGroup::make('Academic')
+                        ->items([
+                            ...StudentResource::getNavigationItems(),
+                            ...TeacherResource::getNavigationItems(),
+                            ...SubjectResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Source')
+                        ->items([
+                            ...CategoryNilaiResource::getNavigationItems(),
+                            ...StudentResource::getNavigationItems(),
+                            ...DepartmentResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Setting')
+                        ->items([
+                            ...PeriodeResource::getNavigationItems(),
+                            NavigationItem::make('Roles')
+                                ->icon('heroicon-o-shield-exclamation')
+                                ->isActiveWhen(fn (): bool => request()->routeIs([
+                                    'filament.admin.resources.roles.index',
+                                    'filament.admin.resources.roles.edit',
+                                    'filament.admin.resources.roles.create',
+                                    'filament.admin.resources.roles.view',
+                                ]))
+                                ->url(fn (): string => '/admin/roles'),
+                            NavigationItem::make('Permissions')
+                                ->icon('heroicon-o-lock-closed')
+                                ->isActiveWhen(fn (): bool => request()->routeIs([
+                                    'filament.admin.resources.permissions.index',
+                                    'filament.admin.resources.permissions.edit',
+                                    'filament.admin.resources.permissions.create',
+                                    'filament.admin.resources.permissions.view',
+                                ]))
+                                ->url(fn (): string => '/admin/permissions'),
+                        ]),
+                ]);
+            });
     }
+    
 
     public function boot(): void
     {
